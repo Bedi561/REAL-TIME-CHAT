229f54bc8a198312eacfbf5c9be42f93e8dc7d47
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2025-03-30 01:00:48 +0530

    all

--- chat-frontend/src/components/SignUpForm.tsx
+++ chat-frontend/src/components/SignUpForm.tsx
@@ -33,8 +33,8 @@ const SignUpForm = () => {
         password,
       };
 
-      // const endpoint = "http://localhost:1337/api/auth/local/register"; // Strapi's register endpoint
-      const endpoint = "https://real-time-chat-n9q7.onrender.com/api/auth/local/register"; // Strapi's register endpoint
+      const endpoint = "http://localhost:1337/api/auth/local/register"; // Strapi's register endpoint
+      // const endpoint = "https://real-time-chat-n9q7.onrender.com/api/auth/local/register"; // Strapi's register endpoint
 
       // Send POST request to the register endpoint
       const response = await axios.post(endpoint, data);
